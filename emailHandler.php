c30d9f6a0019354042862907aca0fa486a74f561
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-10-08 16:20:58 +0300

    Add assesment

--- emailHandler.php
+++ emailHandler.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST["message"]) {
-mail("akram.m.ahmad1989@gmail.com", "Here is the sample subject line",
-$_POST["message"]. "From: akram.m.ahmad1989@gmail.com");
+mail("alsalem.aladdin@gmail.com", "Here is the sample subject line",
+$_POST["message"]. "From: alsalem.aladdin@gmaail.com");
 }
 ?>
